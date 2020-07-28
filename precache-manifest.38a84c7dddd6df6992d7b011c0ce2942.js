878b410a6acbdce7ffe0c1f034215602f400fc77
Navdeep Singh Rathore <razor@pop-os.localdomain>
2020-07-28 09:08:06 +0530

    Updates

---
+++ precache-manifest.38a84c7dddd6df6992d7b011c0ce2942.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063f4ac4fa3fef3539f1b09f7cc09433",
+    "url": "/feedback-prototype/index.html"
+  },
+  {
+    "revision": "a4881d9bb5e5288d0565",
+    "url": "/feedback-prototype/static/css/main.de1c1671.chunk.css"
+  },
+  {
+    "revision": "397a647a522597572612",
+    "url": "/feedback-prototype/static/js/2.adc2246d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/feedback-prototype/static/js/2.adc2246d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4881d9bb5e5288d0565",
+    "url": "/feedback-prototype/static/js/main.e8b28a96.chunk.js"
+  },
+  {
+    "revision": "9553ca811af856b33209",
+    "url": "/feedback-prototype/static/js/runtime-main.0225163f.js"
+  },
+  {
+    "revision": "d78d3a4ea3c4d504a81f5d2bac061c46",
+    "url": "/feedback-prototype/static/media/black1.d78d3a4e.jpg"
+  },
+  {
+    "revision": "4ae186caa3740e0a9e33f12c9200c494",
+    "url": "/feedback-prototype/static/media/pattern.4ae186ca.png"
+  }
+]);
